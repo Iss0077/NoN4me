140cca22ce1770ec9a192ad1081a3c6f8082bcea
Iss0077 <134227156+Iss0077@users.noreply.github.com>
2023-11-16 03:37:13 +0100

    Update loca_tion.php

--- loca_tion.php
+++ loca_tion.php
@@ -2,7 +2,7 @@
 
 $my_file = fopen("location.txt", "w");
 $information = "lat:" . $_GET["lat"] . "\nlong:" . $_GET["long"];
-fwrite($my_file, $information)
-fclose($my_file)
+fwrite($my_file, $information);
+fclose($my_file);
 
 ?>
